32feb3505e093e0e0ead57a9aee3787e981cb17e
OjashMishraA47 <87744719+OjashMishraA47@users.noreply.github.com>
2023-06-28 09:50:46 +0530

    Add files via upload

---
+++ Createbinarytree.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Binary Tree Node structure
+typedef struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+// Function to create a new node
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Function to insert a node into the binary tree
+Node* insertNode(Node* root, int data) {
+    if (root == NULL) {
+        root = createNode(data);
+    } else {
+        if (data <= root->data) {
+            root->left = insertNode(root->left, data);
+        } else {
+            root->right = insertNode(root->right, data);
+        }
+    }
+    return root;
+}
+
+// Function to display the binary tree in inorder traversal
+void inorderTraversal(Node* root) {
+    if (root != NULL) {
+        inorderTraversal(root->left);
+        printf("%d ", root->data);
+        inorderTraversal(root->right);
+    }
+}
+
+int main() {
+    Node* root = NULL;
+    int data;
+    char choice;
+
+    do {
+        printf("Enter the element to insert: ");
+        scanf("%d", &data);
+
+        root = insertNode(root, data);
+
+        printf("Do you want to insert another element? (y/n): ");
+        scanf(" %c", &choice);
+    } while (choice == 'y' || choice == 'Y');
+
+    printf("Binary Tree elements in inorder traversal: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    return 0;
+}
